6257ea557751e1890f790f098b44f362c59c1344
Sadat-Shakeeb <sadatshakeeb@gmail.com>
2026-03-13 01:56:41 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A complete MLOps implementation for Sentiment Analysis including data versioning (DVC), experiment tracking (MLflow), containerization (Docker), scalable deployment (Kubernetes), and monitoring with Prometheus and Grafana.A complete MLOps implementation for Sentiment Analysis including data versioning (DVC), experiment tracking (MLflow), containerization (Docker), scalable deployment (Kubernetes), and monitoring with Prometheus and Grafana.',
+    author='sadat shakeeb',
+    license='',
+)
